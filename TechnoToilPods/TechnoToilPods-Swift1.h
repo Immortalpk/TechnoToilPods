b0a5430aa90ca0cb6efa6dda8b80bf91deb9e27d
Pranav <pranavkhandelwal18@gmail.com>
2021-05-01 18:51:28 +0530

    Added SwiftyLib.podspec

---
+++ TechnoToilPods/TechnoToilPods-Swift1.h
@@ -0,0 +1,12 @@
+//
+//  TechnoToilPods-Swift.h
+//  TechnoToilPods
+//
+//  Created by Pranav on 01/05/21.
+//
+
+#ifndef TechnoToilPods_Swift_h
+#define TechnoToilPods_Swift_h
+
+
+#endif /* TechnoToilPods_Swift_h */
